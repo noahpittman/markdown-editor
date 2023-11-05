b2aea072b7433e558a3694f5d00348e9f97b3083
Noah Pittman <noahpittman00@gmail.com>
2023-11-04 21:47:21 -0400

    init commit

---
+++ Editor.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { BlockNoteEditor } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+
+export default function Editor() {
+	const [isMounted, setIsMounted] = useState<boolean>(false);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	const [markdown, setMarkdown] = useState<string>("");
+
+	const editor: BlockNoteEditor = useBlockNote({
+		onEditorContentChange: (editor) => {
+			const saveBlocksAsMarkdown = async () => {
+				const markdown: string = await editor.blocksToMarkdown(
+					editor.topLevelBlocks
+				);
+				setMarkdown(markdown);
+			};
+			saveBlocksAsMarkdown();
+		},
+		domAttributes: {
+			editor: {
+				class: "h-[calc(100vh-2rem)] p-2 overflow-y-scroll rounded-lg",
+			},
+		},
+	});
+
+	return (
+		<div className="grid grid-cols-2 gap-4 p-4 break-words whitespace-break-spaces overflow-hidden max-h-100vh">
+			<BlockNoteView editor={editor} theme={"dark"} />
+			<pre className="bg-gray-700 max-h-[calc(100vh-2rem)] overflow-auto rounded-lg p-4 whitespace-break-spaces">
+				{markdown}
+			</pre>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,8 @@
-"use client";
+import dynamic from "next/dynamic";
+const Editor = dynamic(() => import("@/Editor"), { ssr: false });
 
-import { useState } from "react";
-import { BlockNoteEditor } from "@blocknote/core";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
-import "@blocknote/core/style.css";
+const App = () => {
+	return <Editor />;
+};
 
-import ReactDom from "react-dom";
-import Markdown from "react-markdown";
-
-export default function App() {
-	const [markdown, setMarkdown] = useState<string>("");
-
-	const editor: BlockNoteEditor = useBlockNote({
-		onEditorContentChange: (editor) => {
-			const saveBlocksAsMarkdown = async () => {
-				const markdown: string = await editor.blocksToMarkdown(
-					editor.topLevelBlocks
-				);
-				setMarkdown(markdown);
-			};
-			saveBlocksAsMarkdown();
-		},
-		domAttributes: {
-			editor: {
-				class: "h-[calc(100vh-2rem)] p-2 overflow-y-scroll rounded-lg",
-			},
-		},
-	});
-
-	return (
-		<div className="grid grid-cols-2 gap-4 p-4 break-words whitespace-break-spaces overflow-hidden max-h-100vh">
-			<BlockNoteView editor={editor} theme={"dark"} />
-			<pre className="bg-gray-700 max-h-[calc(100vh-2rem)] overflow-auto rounded-lg p-4 whitespace-break-spaces">
-				{markdown}
-			</pre>
-		</div>
-	);
-}
+export default App;
